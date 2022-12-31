6e81ff1ff077099726cfe578b26c2c84b6e48514
wiryadev <abrarwiryawan@gmail.com>
2022-12-31 23:52:35 +0700

    create example JSON output

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"crowdfund/user"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	// dsn := "root:@tcp(127.0.0.1:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("Connected")
+
+	// var users []user.User
+	// db.Find(&users)
+
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("=============")
+	// }
+
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/crowdfund?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -17,20 +42,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connected")
-
 	var users []user.User
-	length := len(users)
-	fmt.Println(length)
-
 	db.Find(&users)
 
-	length = len(users)
-	fmt.Println(length)
-
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("=============")
-	}
+	c.JSON(http.StatusOK, users)
 }
